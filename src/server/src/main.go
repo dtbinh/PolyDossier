33e4c443f7ea560128f2c1499ae4a404b93f5e90
Maxime Lavignealavv <duguigne@gmail.com>
2012-06-07 00:27:51 -0400

    Get qui mamarche pour le site :)

--- src/server/src/main.go
+++ src/server/src/main.go
@@ -2,19 +2,39 @@ package main
 
 import (
 	"fmt"
-        "net"
+	"io/ioutil"
+	"net"
 	"net/http"
 	"net/http/fcgi"
+	"crypto/tls"
 )
 
 type FastCGIServer struct{}
 
 func (s FastCGIServer) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	w.Write([]byte("This is an example server.\n"))
+
+	// Nous avons besoin de specifier que nous ne voulons pas
+	// réellement vérifier le Certifica de Poly.
+	tr := &http.Transport{
+	  TLSClientConfig:    &tls.Config{InsecureSkipVerify: true},
+	}
+
+	client := &http.Client{Transport: tr}
+	resp, err := client.Get("https://www4.polymtl.ca/poly/poly.html")
+
+	if err != nil {
+	  w.Write([]byte(err.Error()))
+	}
+
+        defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+
+	w.Write(body)
 }
 
 func main() {
-	fmt.Printf("Starting server")
+	fmt.Println("Starting server")
 	l, _ := net.Listen("tcp", "127.0.0.1:9000")
 	b := new(FastCGIServer)
 	fcgi.Serve(l, b)
