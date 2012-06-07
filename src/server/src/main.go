1f639848ad9f83819753b173b1e503c193b80e95
Maxime Lavignealavv <duguigne@gmail.com>
2012-06-06 21:49:08 -0400

    Ajout d'un serveur de base pour le fast cgi

--- src/server/src/main.go
+++ src/server/src/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+        "net"
+	"net/http"
+	"net/http/fcgi"
+)
+
+type FastCGIServer struct{}
+
+func (s FastCGIServer) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	w.Write([]byte("This is an example server.\n"))
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	fmt.Printf("Starting server")
+	l, _ := net.Listen("tcp", "127.0.0.1:9000")
+	b := new(FastCGIServer)
+	fcgi.Serve(l, b)
 }
