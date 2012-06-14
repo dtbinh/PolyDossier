b75e34045c3a6aaf3b6f79c603574375883a9e74
Eliott Mahou <eliott.mahou@polymtl.ca>
2012-06-14 00:27:55 -0400

    un début de quelque chose concernant le parsing html de la réponse envoyée par le serveur de poly

--- src/server/src/main.go
+++ src/server/src/main.go
@@ -4,77 +4,90 @@
 package main
 
 import (
+	"encoding/xml"
+	"errors"
 	"fmt"
 	"io/ioutil"
-	"errors"
 	"net/http"
-  "time"
+	"time"
 )
 
 const kPolyHost = "https://www4.polymtl.ca"
 
 type RequestError struct {
-  Method string
-  ierror error
+	Method string
+	ierror error
 }
 
 func (r *RequestError) Error() string {
-  return fmt.Sprintf("%s got %s", r.Method, r.ierror)
+	return fmt.Sprintf("%s got %s", r.Method, r.ierror)
 }
 
 var (
-  ErrMethod = errors.New("Method isn't supported")  
+	ErrMethod = errors.New("Method isn't supported")
 )
 
 func main() {
-  http.HandleFunc("/", goHandler)
-    http.ListenAndServe(":http", nil)
+	http.HandleFunc("/", goHandler)
+	http.ListenAndServe(":http", nil)
 }
 
 func goHandler(w http.ResponseWriter, r *http.Request) {
-  t := time.Now()
-  resp, err := goResponse(r)
-  
-  if err != nil {
-    fmt.Println(r.Method, err)
-    return
-  }
-  
-  data, err := ioutil.ReadAll(resp.Body)
-  if err != nil {
-    fmt.Println(r.Method, err)
-    return
-  }
-  
-  w.Write(data)
-  
-  resp.Body.Close()
-  
-  elapsed := time.Since(t)
-  
-  if elapsed < 200 * time.Millisecond {
-    fmt.Println(time.Since(t).String() + "\t" + r.URL.Path)
-  } else {
-    fmt.Println(time.Since(t).String() + "\t" + r.URL.Path + "\t[Trop Long!!!]")
-  }
+	t := time.Now()
+	resp, err := goResponse(r)
+
+	if err != nil {
+		fmt.Println(r.Method, err)
+		return
+	}
+
+	decoder := xml.NewDecoder(resp.Body)
+	token, tErr := xml.Token(nil), error(nil)
+	token, tErr = decoder.Token()
+	for i := 0; tErr == nil; i++ {
+		// if i == 1 {
+		//	decoder.Skip()
+		//} else {*/
+			fmt.Println(token, tErr)
+			token, tErr = decoder.Token()
+		//}
+	}
+
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(r.Method, err)
+		return
+	}
+
+	w.Write(data)
+
+	resp.Body.Close()
+
+	elapsed := time.Since(t)
+
+	if elapsed < 200*time.Millisecond {
+		fmt.Println(time.Since(t).String() + "\t" + r.URL.Path)
+	} else {
+		fmt.Println(time.Since(t).String() + "\t" + r.URL.Path + "\t[Trop Long!!!]")
+	}
 }
 
 func goResponse(r *http.Request) (*http.Response, error) {
-  defaultClient := &http.Client{}
-  
-  // Si on arrive sur la page.
-  if r.URL.Path == "/" {
-    return defaultClient.Get(kPolyHost + "/poly/poly.html")
-  }
-  
-  switch r.Method {
-    case "GET"  :
-      return defaultClient.Get(kPolyHost + r.URL.Path)
-    case "POST" :
-      r.ParseForm()
-      return defaultClient.PostForm(kPolyHost + r.URL.Path, r.Form)
-    default :
-      return nil, &RequestError{Method: r.Method, ierror: ErrMethod}
-  }
-  return nil, nil
+	defaultClient := &http.Client{}
+
+	// Si on arrive sur la page.
+	if r.URL.Path == "/" {
+		return defaultClient.Get(kPolyHost + "/poly/poly.html")
+	}
+
+	switch r.Method {
+	case "GET":
+		return defaultClient.Get(kPolyHost + r.URL.Path)
+	case "POST":
+		r.ParseForm()
+		return defaultClient.PostForm(kPolyHost+r.URL.Path, r.Form)
+	default:
+		return nil, &RequestError{Method: r.Method, ierror: ErrMethod}
+	}
+	return nil, nil
 }
