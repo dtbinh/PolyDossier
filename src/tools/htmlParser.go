86ffd97309cd9ad81c44448f8761838dd797e240
Eliott Mahou <eliott.mahou@polymtl.ca>
2012-08-01 23:05:15 -0400

    un léger nettoyage de code

--- src/tools/htmlParser.go
+++ src/tools/htmlParser.go
@@ -25,11 +25,7 @@ func (p *HTMLParser) Print() {
 	fmt.Println(p.Msg)
 }
 
-// permet de récupérer la valeur d'un champ dans une balise HTML
-// le type retourné est inconnu car il peut autant s'agir d'une 
-// string que d'un nombre
-
-///
+////
 // To get the value of a feild in the HTML
 // 
 //  string : the name of the attribute that we want to get the value of
@@ -37,7 +33,7 @@ func (p *HTMLParser) Print() {
 //  HTMLParameter : pairs of name/value we want in the balise
 // 
 //  return : the value of the wanted parameter
-///
+////
 func (p *HTMLParser) GetValue(wanted string, balise string, champs ...HTMLParameter) interface{} {
 	aNode, errors := html.Parse(p.Data)
 	if errors != nil {
@@ -62,46 +58,6 @@ func printNodeContent(aNode *html.Node, wanted string, balise string) {
 		printNodeContent(nodey, wanted, balise)
 	}
 }
-
-// func ParseHTML(w http.ResponseWriter, r *http.Request) []byte {
-// t := time.Now()
-// resp, err := GoResponse(r)
-// respCpy, err := GoResponse(r)
-
-// if err != nil {
-// fmt.Println(r.Method, err)
-// return WeirdJSON()
-// }
-
-// z := html.NewTokenizer(resp.Body)
-
-// for {
-// tt := z.Next()
-// if tt == html.ErrorToken {
-// fmt.Println("Error token")
-// break
-// }
-// }
-
-// data, err := ioutil.ReadAll(respCpy.Body)
-// if err != nil {
-// fmt.Println(r.Method, err)
-// return WeirdJSON()
-// }
-
-// resp.Body.Close()
-// respCpy.Body.Close()
-
-// elapsed := time.Since(t)
-
-// if elapsed < 200*time.Millisecond {
-// fmt.Println(time.Since(t).String() + "\t" + r.URL.Path)
-// } else {
-// fmt.Println(time.Since(t).String() + "\t" + r.URL.Path + "\t[Trop Long!!!]")
-// }
-
-// return data
-// }
 
 func WeirdJSON() []byte {
 	return []byte("{name:lolsaure}")
